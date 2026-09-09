e7fd9791fe9ff36580efb1b3f25961ed30a55694
Fikri Ramadhan <fikriramadhan1152@gmail.com>
2026-09-09 10:58:30 +0700

    pertemuan 2 php

---
+++ persegipanjang.php
@@ -0,0 +1,9 @@
+<?php
+$panjang =  $_POST['panjang'];
+$lebar = $_POST['lebar'];
+$luas = $panjang*$lebar;
+
+echo"panjang : $panjang <br>";
+echo "lebar : $lebar <br>";
+echo "Luas: $luas <br>"
+?>
